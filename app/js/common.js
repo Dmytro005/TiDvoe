7ecf9c11a89b15cd70eb08f3cc9b6c3c8e432c6a
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2017-12-31 14:11:53 +0200

    Find a solvation for right menu "Client"

--- app/js/common.js
+++ app/js/common.js
@@ -19,12 +19,25 @@ $(function() {
 
 		$(".shadow").toggleClass("shadow-show");
 
+		//Bring Shadow to front
   	if ($(".shadow").css("zIndex") == 1) { $(".shadow").css("zIndex","2") }
   	else if ($(".shadow").css("zIndex") == 2) {$(".shadow").css("zIndex","1") }
 
-		$("#projects-box").toggleClass("projects-box-opened");
-		$("#projects-content").toggleClass("projects-content-opened");
+  	if( $("#projects-box").width().toFixed(0) == 85){
+  		$("#projects-box").animate({width: 325},50, function() {
+  			$("#projects-content").toggleClass("projects-content-opened");
+  		});
+  	}
+  	else if($("#projects-box").width().toFixed(0) == 325){
+			$("#projects-content").toggleClass("projects-content-opened");
+			$("#projects-box").animate({width: 85},50,function() {
+			});
+  	}
 		
+
+		$("#projects-box").toggleClass("projects-box-opened");
+
+
 		// if (zIndex == 1) { 
 		// 		$(".shadow").css("zIndex","2") 
 		// 	}
